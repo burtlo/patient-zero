cd15619c4354d1aa4a140231c23c1304f2e61d71
Franklin Webber <franklin.webber@gmail.com>
2012-12-15 11:26:55 -0600

    Added tile blocks to the collision

--- models/body_collision.rb
+++ models/body_collision.rb
@@ -1,3 +1,51 @@
 class BodyCollision < Metro::Model
 
+  def people
+    scene.updaters.find_all { |person| person.is_a? Person }.map {|p| p.bounds }
+  end
+
+  def bodies
+    people + [scene.hero.bounds ] + blocking_tiles
+  end
+
+  def other_bodies(body)
+    bodies.reject {|b| b == body }
+  end
+
+  def map
+    scene.drawers.find { |drawer| drawer.model == "metro::ui::tile_map" }
+  end
+
+  def layer
+    @layer ||= map.layers.first
+  end
+
+  def blocking_image_indexes
+    [ 2 ]
+  end
+
+  def blocking_tiles
+    @goal_tiles ||= begin
+      layer.data.each_with_index.map do |image_index,position|
+        next unless blocking_image_indexes.include?(image_index)
+        image = layer.tileset_image(image_index)
+        b = layer.position_of_image(image,layer.row(position),layer.column(position))
+        duplicate_and_fix_bounds(b)
+      end.compact
+    end
+  end
+
+  def duplicate_and_fix_bounds(b)
+    Bounds.new left: b.left - (b.right - b.left), top: b.top - (b.bottom - b.top),
+      right: b.right - (b.right - b.left)/2, bottom: b.bottom
+  end
+
+  def move_sprite_by(body,point)
+    new_bounds = body.bounds.shift(point)
+    blocking = other_bodies(body).find {|body| body.intersect?(new_bounds) }
+    unless blocking
+      body.position = body.position + point
+    end
+  end
+
 end
--- scenes/first_scene.rb
+++ scenes/first_scene.rb
@@ -1,11 +1,11 @@
 class FirstScene < GameScene
 
-  draw :hero, position: Game.center
+  draw :hero, position: "400,300"
 
-  draw :person, position: (Game.center - Point.at(128,128))
+  draw :person, position: (Game.center - Point.at(78,128))
   draw :person2, model: "Person", position: (Game.center - Point.at(18,128))
-  draw :person3, model: "Person", position: (Game.center - Point.at(-100,100))
-  draw :person4, model: "Person", position: (Game.center - Point.at(-200,200))
+  draw :person3, model: "Person", position: (Game.center - Point.at(0,150))
+  draw :person4, model: "Person", position: (Game.center - Point.at(-100,200))
 
   draw :map, model: "metro::ui::tile_map", file: "first.json", position: "0,0,-1"
   def viewport
@@ -22,6 +22,8 @@ class FirstScene < GameScene
   draws :disease_control, :body_collision
 
   draw :reached_goal
+
+  draws :body_collision
 
   event :on_hold, KbLeft, GpLeft do
     move_sprite_by(hero,Point.new(-hero.move_amount,0,0))
@@ -40,24 +42,7 @@ class FirstScene < GameScene
   end
 
   def move_sprite_by(body,point)
-    new_bounds = body.bounds.shift(point)
-    blocking = other_bodies(body).find {|body| body.bounds.intersect?(new_bounds) }
-    unless blocking
-      # puts "Moving body: #{body.position} by #{point}"
-      body.position = body.position + point
-    end
-  end
-
-  def people
-    updaters.find_all { |person| person.is_a? Person }
-  end
-
-  def bodies
-    people + [hero]
-  end
-
-  def other_bodies(body)
-    bodies.reject {|b| b == body }
+    body_collision.move_sprite_by(body,point)
   end
 
 
