f69dc523fdd1ed246dd6f25651d6049ee90e7ef6
Franklin Webber <franklin.webber@gmail.com>
2012-12-15 16:12:48 -0600

    Changed Energy to Sick Green

--- scenes/first_scene.rb
+++ scenes/first_scene.rb
@@ -2,7 +2,9 @@ class FirstScene < ActionScene
 
   draw :fps, model: "metro::ui::fps", placement: 'bottom_right'
 
-  draw :energy, position: Point.at(20,20,15), dimensions: Dimensions.of(100,20)
+  draw :energy, position: Point.at(20,20,15), dimensions: Dimensions.of(100,20),
+    color: "rgba(97,186,52,0.6)", fill_color: "rgba(97,186,52,0.6)"
+    
   draws :hero, :energy_loss, :cough_virus, :reached_goal, :panic, :body_collision, :wall_collision
 
   draw :map, model: "metro::ui::tile_map", file: "first.json", position: "0,0,-1"
